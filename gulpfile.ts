26f0e0e2770b0f4de7c125dd17cf22aa52481fb4
Saran Tanpituckpong <sarunta@gmail.com>
2017-11-12 20:30:23 +0700

    :art: Update build, watch and run scripts

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,16 +1,31 @@
+import * as del from 'del';
 import * as gulp from 'gulp';
 import * as ts from 'gulp-typescript';
 import * as uglify from 'gulp-uglify';
+import * as pump from 'pump';
 
-gulp.task('manifest', () => {
+gulp.task('clean:dist', () => del(['./dist/*']));
+
+gulp.task('clean:addon', () => del(['./addon/*']));
+
+gulp.task('clean', ['clean:dist', 'clean:addon']);
+
+gulp.task('manifest', ['clean:dist'], () => {
 	return gulp.src('assets/manifest.json').pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['manifest'], () => {
-	return gulp.src('src/**/*.ts')
-		.pipe(ts())
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
+gulp.task('build', ['clean:dist', 'manifest'], cb => {
+	pump(
+		[
+			gulp.src('src/**/*.ts'),
+			ts(),
+			uglify(),
+			gulp.dest('dist')
+		],
+		cb
+	);
 });
 
-gulp.task('default', ['build']);
+gulp.task('watch', () => gulp.watch('./src/**/*', ['build']));
+
+gulp.task('default', ['watch']);
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ const username = 'gluons';
 let query = encodeURIComponent(`involves:${username} type:issue`);
 let url = `https://github.com/issues?q=${query}`;
 
+let oldInvolvedButton = document.querySelector('#involved-issues-link');
+if (oldInvolvedButton) {
+	oldInvolvedButton.remove();
+}
+
 let involvedButton = document.createElement('a');
+involvedButton.id = 'involved-issues-link';
 involvedButton.classList.add('js-selected-navigation-item', 'subnav-item');
 involvedButton.href = url;
 involvedButton.textContent = 'Involved';
